1ea19809c1744da7e42e3b93095e40173b3245fb
wudongfeng <mr.wudongfeng@gmail.com>
2023-06-18 17:21:39 +0800

    add a demo file

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,8 +13,8 @@ If you have any questions, checkout our [documentation](https://docs.streamlit.i
 forums](https://discuss.streamlit.io).
 
 In the meantime, below is an example of what you can do with just a few lines of code:
+测试提交
 """
-
 
 with st.echo(code_location='below'):
     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
@@ -36,3 +36,5 @@ with st.echo(code_location='below'):
     st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
         .mark_circle(color='#0068c9', opacity=0.5)
         .encode(x='x:Q', y='y:Q'))
+
+## this is a good status
